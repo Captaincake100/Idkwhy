1a2a936d6786c7e8ac37eb155e34837030066f54
Captaincake100 <79469448+Captaincake100@users.noreply.github.com>
2021-02-22 19:05:53 +0000

    upped acc to 90%
    uped acc to 90%

--- git test.py
+++ git test.py
@@ -1,34 +1,32 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
 import numpy
 import tflearn
 import tensorflow
 import random
 import json
 
-import nltk
-from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 with open("intents.json") as file:
     data = json.load(file)
-
 
 words = []
 labels = []
 docs_x = []
 docs_y = []
 
-
 for intent in data["intents"]:
-    for patterns in intent["patterns"]:
-        wrds = nltk.word_tokenize(patterns)
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
         docs_x.append(wrds)
         docs_y.append(intent["tag"])
 
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -41,12 +39,14 @@ out_empty = [0 for _ in range(len(labels))]
 for x, doc in enumerate(docs_x):
     bag = []
 
-    wrd = [stemmer.stem(w) for w in doc]
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
     for w in words:
         if w in wrds:
             bag.append(1)
         else:
             bag.append(0)
+
     output_row = out_empty[:]
     output_row[labels.index(docs_y[x])] = 1
 
@@ -66,5 +66,3 @@ model = tflearn.DNN(net)
 
 model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
 model.save("model.tflearn")
-
-

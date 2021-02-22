6fc44e2d8cd4f872f023e68de9ce5c014cea0922
Captaincake100 <79469448+Captaincake100@users.noreply.github.com>
2021-02-22 17:26:55 +0000

    UPDATE

--- git test.py
+++ git test.py
@@ -9,5 +9,13 @@ from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 with open("intents.json") as file:
+    data = json.load(file)
 
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for patterns in intent["patterns"]:
 
